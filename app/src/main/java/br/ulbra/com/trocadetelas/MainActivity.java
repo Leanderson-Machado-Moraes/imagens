eb379ae83a77b51e55702c82f0ab4ef581e887a1
Leanderson-Machado-Moraes <lele.momo245@gmail.com>
2022-10-10 19:19:19 -0300

    1st commit

---
+++ app/src/main/java/br/ulbra/com/trocadetelas/MainActivity.java
@@ -0,0 +1,38 @@
+package br.ulbra.com.trocadetelas;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.*;
+import android.view.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+        Button bttelaprincipal, bttela2;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            CarregarTelaPrincipal();
+        }
+        public void CarregarTelaPrincipal() {
+            setContentView(R.layout.activity_main);
+            bttela2 = (Button) findViewById(R.id.btnTela2);
+            bttela2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CarregarTela2(); }
+            });
+        }
+
+        public void CarregarTela2() {
+            setContentView(R.layout.tela2);
+            bttelaprincipal = (Button) findViewById(R.id.btnTelaMain);
+            bttelaprincipal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CarregarTelaPrincipal();
+                }
+            });
+        } }
